a92f1f3c32470b47458e69b83111a6dac298609e
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2024-06-08 23:02:58 +0500

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -5,15 +5,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
-import pickle
+import joblib
 
 def test_model_quality():
     quality_data = pd.read_csv('titanic.csv')
     X_quality = quality_data.drop('Survived', axis=1)
     y_quality = quality_data['Survived']
-    with open('pipeline.pkl', 'rb') as file:
-    # Загружаем модель из файла
-        model = pickle.load(file)
+    model = joblib.load('pipeline.pkl')
     y_pred = model.predict(X_quality)
     mse = mean_squared_error(y_quality, y_pred)
     assert mse >= 10, "Model is good enough!"
