8598bd5e396434d614cc41dd1ea46acbe417d945
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2024-06-08 21:54:16 +0500

    Add files via upload

---
+++ test_model.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+import joblib
+
+def test_model_quality():
+    quality_data = pd.read_csv('titanic.csv')
+    X_quality = quality_data[['X']]
+    y_quality = quality_data['y']
+    model = joblib.load('pipeline.pkl')
+    y_pred = model.predict(X_quality)
+    mse = mean_squared_error(y_quality, y_pred)
+    assert mse < 10, "Mean squared error is too high for quality data!
