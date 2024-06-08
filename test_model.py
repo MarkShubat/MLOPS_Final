2e443ddcbafd4f8de77242996f40d1934102c276
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2024-06-08 23:34:34 +0500

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -2,16 +2,16 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 import joblib
 
 def test_model_quality():
     quality_data = pd.read_csv('titanic.csv')
-    X_quality = quality_data.drop('Survived', axis=1)
-    y_quality = quality_data['Survived']
-    model = joblib.load('pipeline.pkl')
-    y_pred = model.predict(X_quality)
-    mse = mean_squared_error(y_quality, y_pred)
-    assert mse >= 10, "Model is good enough!"
+    print(quality_data.info())
+    X = quality_data.drop('Survived', axis=1)
+    y = quality_data['Survived']
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.20, random_state=1) 
+    pipeline = joblib.load('pipeline.pkl')
+    preds = pipeline.predict(X_valid)
+    a = accuracy_score(y_valid, preds)
+    assert a >= 0.75, "Model is bad!"
